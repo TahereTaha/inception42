72cd5efcc09fae10d1cf429252dd302f99cfa931
Taha Tahere <elberkani.taha@gmail.com>
2026-03-19 03:02:06 +0100

    fixed small error with the mariadb user name.

--- srcs/requirements/mariadb/tools/init_db.sql
+++ srcs/requirements/mariadb/tools/init_db.sql
@@ -1,8 +1,8 @@
-CREATE OR REPLACE DATABASE 'mariadb_db' ;
+CREATE OR REPLACE DATABASE mariadb_db ;
 
-CREATE OR REPLACE USER 'mariadb_user'@'%' IDENTIFIED BY 'mariadb_password';
+CREATE OR REPLACE USER mariadb_user@'%' IDENTIFIED BY 'mariadb_password';
 
-GRANT ALL ON 'mariadb_db'.* TO 'mariadb_user'@'%';
+GRANT ALL ON mariadb_db.* TO mariadb_user@'%';
 
 FLUSH PRIVILEGES;
 
