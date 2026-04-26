de6fc4d0962a895cb9fb074b77b19ae3316189e8
taha <taha@tatahere.42.fr>
2026-04-26 18:39:33 +0200

    adding a editor user to the wordpress.

--- srcs/requirements/wordpress/tools/init_wp_cli.sh
+++ srcs/requirements/wordpress/tools/init_wp_cli.sh
@@ -14,7 +14,12 @@ if [ $? -eq 1 ]; then
 		--admin_user="$WP_ADMIN_USER" \
 		--admin_password="$WP_ADMIN_PASSWORD" \
 		--admin_email="$WP_ADMIN_EMAIL"
+
+	wp user create --allow-root "$WP_USER" "$WP_USER_EMAIL" --role=author \
+			--user_pass="$WP_USER_PASSWORD"
 fi
+
+
 
 exec php-fpm83 -F 
 
